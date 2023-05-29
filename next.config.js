5070f0973c6afa6ea2d9e45d0bfb45e9b6b448db
Michael Opoku <michaelopoku790@gmail.com>
2023-05-29 23:25:11 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 }
 
 module.exports = nextConfig
 
-module.exports = {
-    typescript: {
-      // !! WARN !!
-      // Dangerously allow production builds to successfully complete even if
-      // your project has type errors.
-      // !! WARN !!
-      ignoreBuildErrors: true,
-    },
-  }
