25b68d54c4f8dd6e8b086add857519bc3e8192c0
Michael Opoku <michaelopoku790@gmail.com>
2023-06-14 15:30:54 +0000

    Add files via upload

--- app/components/Experience.tsx
+++ app/components/Experience.tsx
@@ -15,9 +15,7 @@ const Details = ({position, company, companyLink, time, address, work}:any) => {
             <h3 className="capitalize font-bold text-2xl sm:text-xl xs:text-lg ">{position}&nbsp;<a href={companyLink}
             target="_blank" className="text-primary dark:text-primaryDark capitalize"
             >@{company}</a> </h3>
-            <span className="capitalize font-medium text-dark/75 dark:text-light/75 xs:text-sm  ">
-                {time} | {address}
-            </span>
+          
             <p className="font-medium w-full md:text-sm ">
                 {work}
             </p>
